e7f74729e69efb0d8a987fc4d53fc8da513f0cea
Joe Petrakovich <jpetrakovich@gmail.com>
2020-08-12 09:00:02 -0500

    removed card history API call as it wasn't ever used.  An unfinished feature from days gone by...

--- LeanPocket/src/main/java/com/appshroom/leanpocket/api/retrofit/RetroLeanKitApi.java
+++ LeanPocket/src/main/java/com/appshroom/leanpocket/api/retrofit/RetroLeanKitApi.java
@@ -2,25 +2,14 @@ package com.appshroom.leanpocket.api.retrofit;
 
 import com.appshroom.leanpocket.dto.AddCardReplyData;
 import com.appshroom.leanpocket.dto.Card;
-import com.appshroom.leanpocket.dto.CardHistory;
-import com.appshroom.leanpocket.dto.Comment;
-import com.appshroom.leanpocket.dto.DeleteCardsReplyData;
-import com.appshroom.leanpocket.dto.Lane;
 import com.appshroom.leanpocket.dto.LeanKitResponse;
-import com.appshroom.leanpocket.dto.LeanKitTreeifiedLane;
 import com.appshroom.leanpocket.dto.UpdateCardReplyData;
-
-import java.util.List;
 
 import retrofit.Callback;
 import retrofit.http.Body;
-import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Path;
 
-/**
- * Created by jpetrakovich on 8/16/13.
- */
 public interface RetroLeanKitApi {
 
     @POST("/Board/{boardId}/AddCard/Lane/{laneId}/Position/{position}")
@@ -28,21 +17,11 @@ public interface RetroLeanKitApi {
                  @Path("boardId") String boardId,
                  @Path("laneId") String laneId,
                  @Path("position") int position,
-                 Callback<LeanKitResponse<AddCardReplyData>> cb); //TODO: do something like I did in volley to handle WIP stuff
+                 Callback<LeanKitResponse<AddCardReplyData>> cb);
 
     @POST("/Board/{boardId}/UpdateCard")
     void updateCard(@Body Card card,
                     @Path("boardId") String boardId,
                     Callback<LeanKitResponse<UpdateCardReplyData>> cb);
-
-    @POST("/Board/{boardId}/DeleteCards")
-    void deleteCards(@Body List<String> cardIds,
-                     @Path("boardId") String boardId,
-                     Callback<LeanKitResponse<DeleteCardsReplyData>> cb);
-
-    @GET("/Card/History/{boardId}/{cardId}")
-    void getCardHistory(@Path("boardId") String boardId,
-                        @Path("cardId") String cardId,
-                        Callback<LeanKitResponse<List<CardHistory>>> cb);
 
 }
